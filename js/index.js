471e0faef3a8b73895d989b2847dd96ff1ea9e06
Neil Ashton <neil.m.ashton@gmail.com>
2017-01-13 12:35:28 -0500

    Resize map on window resize

--- js/index.js
+++ js/index.js
@@ -1,40 +1,66 @@
 var RATIO = 200 / 490;
+var X_TRANSLATE_SCALE = 0.575;
+
+var $container = d3.select('#map-container');
+var $svg = d3.select('#mount').append('svg');
+var projection = d3.geoConicConformal()
+  .parallels([34 + 20 / 60, 36 + 10 / 60])
+  .rotate([79, -33 - 45 / 60]);
+var path = d3.geoPath().projection(projection);
 
 function getContainerWidth () {
-  return d3.select('#map-container')
+  return $container
     .node()
     .getBoundingClientRect()
     .width;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+function getDimensions () {
   var width = getContainerWidth();
   var height = width * RATIO;
-  var colors = d3.schemeCategory20;
-  var projection = d3.geoConicConformal()
-  .parallels([34 + 20 / 60, 36 + 10 / 60])
-  .rotate([79, -33 - 45 / 60])
-  .scale(width * 6)
-  .translate([width * 0.575, height]);
-  var path = d3.geoPath().projection(projection);
+  return {
+    width: width,
+    height: height
+  };
+}
 
-  var $svg = d3.select('#mount')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height);
+function refreshProjection () {
+  var dimensions = getDimensions();
+  projection
+    .scale(dimensions.width * 6)
+    .translate([dimensions.width * X_TRANSLATE_SCALE, dimensions.height]);
+}
+
+function resizeSVG () {
+  var dimensions = getDimensions();
+  $svg
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.width * RATIO);
+}
+
+function drawMap (features) {
+  $svg.selectAll('path')
+    .data(features)
+      .attr('d', path)
+    .enter()
+      .append('path')
+      .attr('d', path);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  resizeSVG();
+  refreshProjection();
 
   d3.queue()
-  .defer(d3.json, 'data/2011.geojson')
-  .defer(d3.json, 'data/2016.geojson')
-  .await(function (err, data2011, data2016) {
-    var features = data2011.features;
+    .defer(d3.json, 'data/2011.geojson')
+    .defer(d3.json, 'data/2016.geojson')
+    .await(function (err, data2011, data2016) {
+      drawMap(data2016.features);
 
-    window.f = data2011.features;
-
-    var $districts = $svg.selectAll('path')
-    .data(features)
-    .enter()
-    .append('path')
-    .attr('d', path);
+      window.addEventListener('resize', function () {
+        resizeSVG();
+        refreshProjection();
+        drawMap(data2016.features);
+      });
   });
 });
