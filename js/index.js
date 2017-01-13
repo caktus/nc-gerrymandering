8f249f997c64230bedd4d779fe3a2317f573962b
Neil Ashton <neil.m.ashton@gmail.com>
2017-01-13 10:48:07 -0500

    Displaying the basic SVG map

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
-var mount = document.getElementById('mount');
+var width = 960;
+var height = 500;
+var colors = d3.schemeCategory20;
+var projection = d3.geoConicConformal()
+  .parallels([34 + 20 / 60, 36 + 10 / 60])
+  .rotate([79, -33 - 45 / 60])
+  .scale(6102.002295938357)
+  .translate([570.5880508434078, 431.7927213940179]);
+var path = d3.geoPath().projection(projection);
+
+var $svg = d3.select('#mount')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
 d3.queue()
   .defer(d3.json, 'data/2011.geojson')
   .defer(d3.json, 'data/2016.geojson')
   .await(function (err, data2011, data2016) {
-    /* geo-magic goes here... */
+    var features = data2011.features;
+
+    window.f = data2011.features;
+
+    var $districts = $svg.selectAll('path')
+      .data(features)
+      .enter()
+      .append('path')
+        .attr('d', path)
+        .style('fill', function (d, i) { return colors[i]; });
   });
