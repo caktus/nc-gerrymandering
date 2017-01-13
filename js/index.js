de6e96fe05fe29c8baf5663a535f71fc8cb537d0
Neil Ashton <neil.m.ashton@gmail.com>
2017-01-13 10:22:53 -0500

    Simplifying JS and making it actually do what it's supposed to

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-document.onload(function () {
-  var mount = document.getElementById('mount');
-});
+var mount = document.getElementById('mount');
+
+d3.queue()
+  .defer(d3.json, 'data/2011.geojson')
+  .defer(d3.json, 'data/2016.geojson')
+  .await(function (err, data2011, data2016) {
+    /* geo-magic goes here... */
+  });
