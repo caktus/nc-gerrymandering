ffaea5d3c9d729d07380d9eb4fe6af76182a5c36
Neil Ashton <neil.m.ashton@gmail.com>
2017-01-13 12:01:03 -0500

    Merge with master

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-var width = 960;
-var height = 500;
+var width = 480;
+var height = 250;
 var colors = d3.schemeCategory20;
 var projection = d3.geoConicConformal()
   .parallels([34 + 20 / 60, 36 + 10 / 60])
   .rotate([79, -33 - 45 / 60])
-  .scale(6102.002295938357)
-  .translate([570.5880508434078, 431.7927213940179]);
+  .scale(3051.0011479691784)
+  .translate([285.2940254217039, 215.89636069700896]);
 var path = d3.geoPath().projection(projection);
 
 var $svg = d3.select('#mount')
